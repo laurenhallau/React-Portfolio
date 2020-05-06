16ae43a5a16bcc29fad8555130a749fd5d909680
Lauren Hallau <lhallau@gmail.com>
2020-05-06 16:10:35 -0500

    adding wrapper and footer components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./components/pages/About";
+import About from "../src/pages/About";
 import NavTabs from "./components/NavTabs";
-import Projects from "./components/pages/Projects";
-import Resume from "./components/pages/Resume";
-import Contact from "./components/pages/Contact";
-import './App.css';
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
+// import './App.css';
 
 function App() {
   return (
     <Router>
       <div>
         <NavTabs />
-        <Route exact path="/" component={About} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/resume" component={Resume} />
-        <Route exact path="/contact" component={Contact} />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/contact" component={Contact} />
+        </Wrapper>
+        <Footer />
       </div>
     </Router>
   );
