bc8f4c092d51332dd62726f85b56f021603be490
Lauren Hallau <lhallau@gmail.com>
2020-05-09 14:04:58 -0500

    updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Document, Page, pdfjs } from 'react-pdf';
 
 
-import lwh from '../images/lwh.pdf';
+import resume2 from '../images/resume2.pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 // function Resume () {
@@ -19,7 +19,7 @@ export default class Resume extends Component {
   render() {
     return (
       <Document className="resumePDF"
-        file={lwh}
+        file={resume2}
         onLoadSuccess={this.onDocumentLoadSuccess}
       >
      <Page  pageNumber={1} />
