e184f137dc79c8c4d40c9e2e4ae73a8b8cf0921f
Lauren Hallau <lhallau@gmail.com>
2020-05-09 15:18:58 -0500

    heroku debugging

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,29 +1,29 @@
-import React, { Component } from "react";
-import { Document, Page, pdfjs } from 'react-pdf';
+// import React, { Component } from "react";
+// import { Document, Page, pdfjs } from 'react-pdf';
 
 
-import resume2 from '../images/resume2.pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+// import resume2 from '../images/resume2.pdf';
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-// function Resume () {
+// // function Resume () {
+// //     return (
+// //         <div>
+// //           <h2>Resume</h2>
+// //         </div>
+// //     );
+// // }
+
+// // export default Resume;
+
+// export default class Resume extends Component {
+//   render() {
 //     return (
-//         <div>
-//           <h2>Resume</h2>
-//         </div>
+//       <Document className="resumePDF"
+//         file={resume2}
+//         onLoadSuccess={this.onDocumentLoadSuccess}
+//       >
+//      <Page  pageNumber={1} />
+//       </Document>
 //     );
+//   }
 // }
-
-// export default Resume;
-
-export default class Resume extends Component {
-  render() {
-    return (
-      <Document className="resumePDF"
-        file={resume2}
-        onLoadSuccess={this.onDocumentLoadSuccess}
-      >
-     <Page  pageNumber={1} />
-      </Document>
-    );
-  }
-}
